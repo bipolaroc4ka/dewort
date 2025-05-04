13bc3ed56efe88da15602cb104985117477bee2b
Ilya Sychev <58861662+bipolaroc4ka@users.noreply.github.com>
2025-05-04 22:59:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -211,7 +211,7 @@ document.addEventListener("keydown", (e) => {
 function initKeyboard() {
     const keys = [
         ['Q','W','E','R','T','Z','U','I','O','P','Ü'],
-        ['A','S','D','F','G','H','J','K','L','Ö','Ä'],
+        ['A','S','D','F','G','H','J','K','L','Ö','Ä','ß'],
         ['Enter','Y','X','C','V','B','N','M','Backspace']
     ];
 
